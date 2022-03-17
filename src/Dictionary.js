d6db6b56b31329ed44c8fee225447098bda9441e
tavia929 <86624687+tavia929@users.noreply.github.com>
2022-03-16 20:40:44 -0400

    rendered meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 
 export default function Dictionary() {
     let [keyword, setKeyword] = useState("");
+    let [results, setResults] = useState(null);
     
     function handleResponse(response) {
         console.log(response.data[0]);
+        setResults(response.data[0]);
     }
 
     function search(event) {
         event.preventDefault();
+
+        // documentation: https://dictionaryapi.dev/
 
         let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}/`;
         axios.get(apiUrl).then(handleResponse);
@@ -27,6 +32,7 @@ export default function Dictionary() {
             onChange={handleKeywordChange} 
             autoFocus={true} />
         </form>
+        <Results results={results} />
         </div>
     );
 }
